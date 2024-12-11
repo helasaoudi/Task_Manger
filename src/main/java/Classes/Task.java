d41f0a59639be27b38d897d5e7d2ec732ea6be8d
Hela saoudi <helasaoudi024@gmail.com>
2024-12-11 10:14:14 +0100

    first commit

---
+++ src/main/java/Classes/Task.java
@@ -0,0 +1,55 @@
+package Classes;
+
+import java.util.Date;
+
+public class Task {
+    private int id;
+    private String libelle;
+    private Date dateRéalisation;
+    private double prixHeure;
+
+    public Task(int id, String libelle, Date dateRéalisation, double prixHeure) {
+        this.id = id;
+        this.libelle = libelle;
+        this.dateRéalisation = dateRéalisation;
+        this.prixHeure = prixHeure;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public Date getDateRéalisation() {
+        return dateRéalisation;
+    }
+
+    public void setDateRéalisation(Date dateRéalisation) {
+        this.dateRéalisation = dateRéalisation;
+    }
+
+    public double getPrixHeure() {
+        return prixHeure;
+    }
+
+    public void setPrixHeure(double prixHeure) {
+        this.prixHeure = prixHeure;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{id=" + id + ", libelle='" + libelle + "', dateRéalisation=" + dateRéalisation + ", prixHeure=" + prixHeure + "}";
+    }
+}
+
